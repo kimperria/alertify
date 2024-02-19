c65efa0901203e24e221d5b0e55b5ab78982c75d
kimperria <kimwaweru100@gmail.com>
2024-02-19 22:46:49 +0300

    ch(Settings): update settings folder structure
    add dev and prod settings config
    update asgi and wsgi files

---
+++ settings/prod.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# add your production host here
+ALLOWED_HOSTS += []  # noqa
+
+# add your production whitelist here
