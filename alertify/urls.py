3379f3c2f28060aa0fd1ed11233e535a9b73605b
kimperria <kimwaweru100@gmail.com>
2024-02-20 15:55:18 +0300

    ft(Google Auth x Swagger): configure Social Auth and enable swagger API doc
    install drf_yasg
    configure and enable google social auth

--- alertify/urls.py
+++ alertify/urls.py
@@ -19,10 +19,36 @@ from django.contrib import admin
 from django.urls import (
     include,
     path,
+    re_path,
+)
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Alertify API",
+        default_version="v1",
+        description="API Documentation for Alertify Project",
+        contact=openapi.Contact(email="kimperriatest@gmail.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/app/", include("app.urls")),
-    path("api-auth/", include("rest_framework.urls")),
+    re_path(r"^auth/", include("drf_social_oauth2.urls", namespace="drf")),
+    # Swagger urls
+    path(
+        "docs/swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
