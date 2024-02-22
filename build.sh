82b6b4a4b6e4a3eff8f4036e807ecca1dae3e26e
kimperria <kimwaweru100@gmail.com>
2024-02-22 23:06:53 +0300

    ch(Render): add debug statements

--- build.sh
+++ build.sh
@@ -3,12 +3,15 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
+echo "Installing Dependacies"
 pipenv install
 
 # Apply any outstanding database migrations
+echo "Running Migrations"
 python manage.py migrate
 
 # Create superuser
+echo "Creating super user"
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input
